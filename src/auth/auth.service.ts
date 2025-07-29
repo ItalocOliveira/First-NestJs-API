b056afd85fee91462bbe008a008be769f63515e6
italo <italocaua19506@gmail.com>
2025-07-29 12:46:15 -0300

    Outdated prisma solved

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "generated/prisma/runtime/library";
 
 @Injectable()
 export class AuthService{
@@ -11,19 +12,47 @@ export class AuthService{
         // generate password hash
         const hash = await argon.hash(dto.password);
         // save new user on db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            }
-        });
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
 
-        delete user.hash;
-        // return saved user
-        return user;
+            delete user.hash;
+            // return saved user
+            return user;
+        }   catch(error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Credentials taken',
+                    );
+                }
+            }
+            throw error;
+        }
+
     }; 
 
-    signin() {
+    async signin(dto: AuthDto) {
+        // find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        })
+        // if user doesnt exist, throw exception
+        
+        // compare password
+
+        // if password incorrect, throw exception
+
+        // send back user
+
+        
+
         return {msg: "signin"}
     };
 }
