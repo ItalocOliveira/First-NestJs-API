9a578e643387046c43cf795bb9e52cac87039a40
italo <italocaua19506@gmail.com>
2025-08-01 11:26:28 -0300

    guard logic seted up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,6 +25,7 @@ export class AuthService{
                     hash,
                 }
             });
+            return this.signToken(user.id, user.email);
         }   catch(error) {
             if(error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
